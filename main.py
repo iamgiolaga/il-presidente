1131cc40c4ad8942d6b30e151f610a15e47459c4
Giovanni Laganà <giovanni.lagana1995@gmail.com>
2023-09-30 00:48:34 +0200

    setup code

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+from datetime import datetime, timedelta
+
+date_time_format = '%Y-%m-%dT%H:%M:%SZ'
+
+def get_match_day_id():
+    url = "https://www.legaseriea.it/api/season/157617/championship/A/matchday?lang=it"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()['data']
+            for item in data:
+                if item["category_status"] == "TO BE PLAYED":
+                    return item["id_category"]
+            return
+        else:
+            print(f'Errore: La richiesta ha restituito il codice di stato {response.status_code}')
+
+    except requests.exceptions.RequestException as e:
+        print(f'Errore: Impossibile eseguire la richiesta GET: {e}')
+
+def get_next_day(match_day_id):
+    url = f'https://www.legaseriea.it/api/stats/live/match?extra_link&order=oldest&lang=it&season_id=157617&page=1&limit=20&pag&match_day_id={match_day_id}'
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            matches = response.json()['data']['matches']
+            return matches[0]['date_time']
+        else:
+            print(f'Errore: La richiesta ha restituito il codice di stato {response.status_code}')
+
+    except requests.exceptions.RequestException as e:
+        print(f'Errore: Impossibile eseguire la richiesta GET: {e}')
+
+if __name__ == '__main__':
+    match_day_id = get_match_day_id()
+    next_day = get_next_day(match_day_id)
+    date_time = datetime.strptime(next_day, date_time_format)
+    current_time = datetime.utcnow()
+    time_difference = date_time - current_time
+    if time_difference == timedelta(hours=2):
+        print("Ok")
